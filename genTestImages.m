3532017e54bfd7d9e4e3829af08c6a87b313f6d8
BillyO24 <100812335+BillyO24@users.noreply.github.com>
2023-12-04 14:07:52 -0600

    Phantom 1
    Generated phantom one and turned file into function with parameters for adjusting later. Need to add parameter variables to certain image functions for user adjustment later.

--- genTestImages.m
+++
@@ -1,8 +0,0 @@
-mat = 255*ones(250,250,'uint8');
-IM2 = padarray(mat,[5 5],0,'both');
-colorCode = colormap*255;
-IM2 = insertShape(IM2,"filled-circle",[130 130 90],ShapeColor="cyan");
-IM2 = insertShape(IM2,"circle",[130 130 90],LineWidth=3,ShapeColor="black");
-IM2 = insertShape(IM2,"filled-rectangle",[117 60 25 140],LineWidth=3,ShapeColor="white");
-IM2 = insertShape(IM2,"rectangle",[117 60 25 140],LineWidth=3,ShapeColor="black");
-imshow(IM2);
