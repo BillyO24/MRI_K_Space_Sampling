d4f0d4038809cabe913b08cda82f1dfc3d9d627a
BillyO24 <100812335+BillyO24@users.noreply.github.com>
2023-12-04 15:13:08 -0600

    Reorganization
    Placed test image generation functions in same file as local functions under main driver function.

--- genPhantom1.m
+++
@@ -1,9 +0,0 @@
-function p1 = genPhantom1(N, r, w, h) 
-    mat = zeros(250,250,'uint8');
-    IM2 = insertShape(mat,"filled-circle",[130 130 90],Opacity=0.5);
-    IM2 = insertShape(IM2,"filled-rectangle",[117 60 25 140],Opacity=1);
-    IM2 = rgb2gray(IM2);
-    imwrite(IM2, 'phantom1.jpg');
-    imshow(IM2);
-    p1 = IM2; 
-end
--- main.m
+++ main.m
@@ -1,3 +1,21 @@
+function main()
+    [IM1, IM2] = genTestImages();
+end
+
+function [i1, i2] = genTestImages()
+    i1 = genPhantom1();
+    i2 = genPhantom2();
+end
+
+function p1 = genPhantom1(N, r, w, h) 
+    mat = zeros(250,250,'uint8');
+    IM2 = insertShape(mat,"filled-circle",[130 130 90],Opacity=0.5);
+    IM2 = insertShape(IM2,"filled-rectangle",[117 60 25 140],Opacity=1);
+    IM2 = rgb2gray(IM2);
+    imwrite(IM2, 'phantom1.jpg');
+    p1 = IM2; 
+end
+
 function p2 = genPhantom2(N, r1, r2, r3, r4, r5)
     mat = zeros(250,250,'uint8');
 
@@ -10,6 +28,5 @@ function p2 = genPhantom2(N, r1, r2, r3, r4, r5)
 
     IM2 = rgb2gray(IM2);
     imwrite(IM2, 'phantom2.jpg');
-    imshow(IM2);
     p2 = IM2;
 end
