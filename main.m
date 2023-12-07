27eaf50b096eb964b18a5a4544812fc8a9b2ddec
BillyO24 <100812335+BillyO24@users.noreply.github.com>
2023-12-07 11:03:02 -0600

    Working on GUI
    Code for phantom generation and image sampling/reconstruction is working. Developing GUI and integrating with main code.

--- main.m
+++ main.m
@@ -1,18 +1,9 @@
 function main()
     %[IM1, IM2] = genTestImages();
     IM1 = phantom('Modified Shepp-Logan', 256);
-    IM1_C = cartesian(IM1, 128, 64);
+    IM1_C = cartesian(IM1, 100, 100);
 end
 
-% Keep just in case we need for reconstructing image after sampling
-function m = magnitude(F)
-    mag = sqrt(real(F).^2 + imag(F).^2);
-    phase = atan2(imag(F),real(F));
-    re = mag .* cos(phase);
-    im = mag .* sin(phase);
-
-    m = re + i*im;
-end
 
 function [i1, i2] = genTestImages()
     i1 = genPhantom1();
@@ -92,6 +83,183 @@ function p2 = genPhantom2()
     p2 = IM7;
 end
 
+% Cartesian sampling and image reconstruction
+function p = cartesian(IM, nlines, ppline)
+    numberOfLines = nlines;
+    pointsPerLine = ppline;
+
+    % Generate Cartesian grid
+    [x, y] = meshgrid(-numberOfLines:pointsPerLine, -numberOfLines:pointsPerLine);
+
+    % Calculate radial distance from the center
+    radialDistance = sqrt(x.^2 + y.^2);
+
+    % Creating a circular mask with radius 15
+    circularMask = (radialDistance < 15);
+
+    % Performing Fourier Transform
+    fftResult = fftshift(fft2(circularMask));
+
+    % Computing the log magnitude spectrum
+    logMagnitudeSpectrum = log(1 + abs(fftResult));
+
+    % Visualize Sampled K-Space for Radial
+    %imshow(im2uint8(logMagnitudeSpectrum / max(logMagnitudeSpectrum(:))), [], 'parent', app.SampledKSpaceImage);
+
+    numLines = numberOfLines;
+    numPoints = pointsPerLine;
+    % Defining ranges for cropping
+    startX = int64(640 - (numLines * 2.5));
+    startY = int64(640 - (numPoints * 2.5));
+    if startX == 0
+        startX = 1;
+    end
+    if startY == 0
+        startY = 1;
+    end
+    endX = int64(640 + (numLines * 2.5));
+    endY = int64(640 + (numPoints * 2.5));
+
+    %disp(startX);
+    %disp(startY);
+    %disp(endX);
+    %disp(endY);
+
+    imData = IM;
+    imData = im2gray(imData);
+
+    % Fourier transform with zero padding
+    fourierTransform = fft2(imData, 1280, 1280);
+    shiftedKSpace = fftshift(fourierTransform);
+
+    % Cropping the k-space
+    croppedKSpace = shiftedKSpace(startX:endX, startY:endY);
+
+    % Inverse Fourier transform
+    reconstruction = ifft2(croppedKSpace);
+
+    % Cropping the reconstruction to specified size
+    croppedReconstruction = reconstruction(1:numLines, 1:numPoints);
+
+    % Scaling and resizing the image
+    absScaled = abs(croppedReconstruction);
+    stretchedImage = imresize(absScaled, [256, 256]);
+
+    % Generating Sampled K-Space image for Cartesian sampling
+    targetSize = [numLines * 5, numPoints * 5];
+    win1 = centerCropWindow2d(size(shiftedKSpace), targetSize);
+    B1 = imcrop(abs(log(shiftedKSpace)), win1);
+    B1 = imresize(B1, [256, 256]);
+    %imshow(B1, [], 'parent', app.SampledKSpaceImage);
+    idiff = stretchedImage;
+    % Display the reconstructed image for Cartesian
+    %imshow(idiff, [], 'parent', app.ReconstructedImage);
+    imshow(idiff, []);
+    p = idiff;
+end
+
+% Radial sampling and image reconstruction
+function p = radial(IM, nlines, ppline)
+    numberOfLines = nlines;
+    pointsPerLine = ppline;
+
+    % Generate Cartesian grid
+    [x, y] = meshgrid(-numberOfLines:pointsPerLine, -numberOfLines:pointsPerLine);
+
+    % Calculate radial distance from the center
+    radialDistance = sqrt(x.^2 + y.^2);
+
+    % Creating a circular mask with radius 15
+    circularMask = (radialDistance < 15);
+
+    % Performing Fourier Transform
+    fftResult = fftshift(fft2(circularMask));
+
+    % Computing the log magnitude spectrum
+    logMagnitudeSpectrum = log(1 + abs(fftResult));
+
+    % Visualize Sampled K-Space for Radial
+    %imshow(im2uint8(logMagnitudeSpectrum / max(logMagnitudeSpectrum(:))), [], 'parent', app.SampledKSpaceImage);
+
+    % Read the phantom image
+    %phantomImage = imread('./phantom.png');
+    phantomImage = IM;
+
+    if (numberOfLines >= 16) && (numberOfLines < 64)
+        if(pointsPerLine >= 16) && (pointsPerLine < 64)
+            theta1 = 0:10:170;
+        else
+            theta1 = 0:10:180;
+        end
+        % Perform Radon transform
+        [R1, ~] = radon(phantomImage, theta1);
+        % size(R1, 2); returns number of columns in matrix R1
+        % size(R1, 1); returns number of rows in matrix R2
+
+        % Interpolate to 512 angles
+        P512 = phantomImage;
+        [~, ~] = radon(P512, theta1);
+        %size(R512, 1); returns the number of rows in matrix R512
+        outputSize = max(size(phantomImage));
+        dTheta1 = theta1(2) - theta1(1);
+        I1 = iradon(R1, dTheta1, outputSize);
+
+        % Display the reconstructed image for Radial sampling
+        idiff = I1;
+        %imshow(idiff, [], 'parent', app.ReconstructedImage);
+        imshow(idiff, []);
+       elseif (numberOfLines >= 64) && (numberOfLines <= 128)
+           if (pointsPerLine >= 64) && (pointsPerLine <= 128)
+           theta2 = 0:5:175;
+           else
+               theta2 = 0:4.9:180;
+           end
+    
+           % Perform Radon transform
+           [R2, ~] = radon(phantomImage, theta2);
+           %size(R2, 2);  returns number of column in matrix R2
+           % size(R2, 1); returns number of rows in matrix R2
+    
+           % Interpolate to 512 angles
+           P512 = phantomImage;
+           [~, ~] = radon(P512, theta2);
+           % size(R512, 1); returns number of rows in matrix R512
+           outputSize = max(size(phantomImage));
+           dTheta2 = theta2(2) - theta2(1);
+           I2 = iradon(R2, dTheta2, outputSize);
+           idiff = I2;
+        
+           % Display the reconstructed image of radial sampling
+           %imshow(idiff, [], 'parent', app.ReconstructedImage);
+           imshow(idiff, []);
+       else
+           if(numberOfLines > 128) && (pointsPerLine > 128)
+               theta3 = 0:2:178;
+           else
+               theta3 = 0:2.5:178;
+           end
+
+           % Perform Radon transform
+           [R3, ~] = radon(phantomImage, theta3);
+           %size(R3, 2); returns number of columns in matrix R3
+           %size(R3, 1); returns number of rows in matrix R3
+
+           % Interpolate to 512 angles
+           P512 = phantomImage;
+           [~, ~] = radon(P512, theta3);
+           %size(R512, 1); returns number of rows in matrix R512
+           outputSize = max(size(phantomImage));
+           dTheta3 = theta3(2) - theta3(1);
+           I3 = iradon(R3, dTheta3, outputSize);
+           idiff = I3;
+
+           % Display the reconstructed image of radial sampling
+           %imshow(idiff, [], 'parent', app.ReconstructedImage);
+           imshow(idiff, []);
+       end
+end
+
+%{
 function result = cartesian(IM, slines, spoints)
     size_img = length(IM); % size of original phantom
     % ratios of phantom dimensions to sampling grid dimensions
@@ -107,6 +275,15 @@ function result = cartesian(IM, slines, spoints)
     F_log = log(1 + FT);
     %imshow(F_log, []);
     % sample k-space
-    %smpl = interp2(FT, ,);
+    SampleIM = zeros(new_dim(1), new_dim(2), 'uint8');
+    
+    pointStep = ;
+    lineStep = ;
+    for i = 0:slines
+        for j = 0:spoints
+            
+        end
+    end
     result = IM2;
 end
+%}
